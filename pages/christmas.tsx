abf939fc20ad5951159a26423549fcd35bc6d994
Rob Dearling <robbie-jay.dearling@lexisnexisrisk.com>
2024-12-16 11:26:48 +0000

    chore: Formatting

--- pages/christmas.tsx
+++ pages/christmas.tsx
@@ -29,57 +29,64 @@ const triviaQuestions: ChristmasTriviaQuestion[] = [
   {
     question: "What do children in Sweden leave out for Santa?",
     answer: "A cup of coffee so he doesn’t get tired",
-    day: 12
+    day: 12,
   },
   {
     question: "How many tips do traditional snowflakes have?",
     answer: "6",
-    day: 13
+    day: 13,
   },
   {
     question: "Who wrote the classic story A Christmas Carol?",
     answer: "Charles Dickens",
-    day: 14
+    day: 14,
   },
   {
-    question: "In Home Alone, what are the names of the two burglars trying to break into Kevin McCallister's house?",
+    question:
+      "In Home Alone, what are the names of the two burglars trying to break into Kevin McCallister's house?",
     answer: "Harry and Marv",
-    day: 15
+    day: 15,
   },
   {
-    question: "During WW1, a famous 'Christmas Truce' took place, when soldiers agreed to a cease fire to celebrate the occasion and play football. But which year did it take place?",
+    question:
+      "During WW1, a famous 'Christmas Truce' took place, when soldiers agreed to a cease fire to celebrate the occasion and play football. But which year did it take place?",
     answer: "1914",
-    day: 16
+    day: 16,
   },
   {
-    question: "Which country is often credited with the tradition of hanging stockings by the fireplace for Santa Claus to fill with gifts?",
+    question:
+      "Which country is often credited with the tradition of hanging stockings by the fireplace for Santa Claus to fill with gifts?",
     answer: "United States",
-    day: 17
+    day: 17,
   },
   {
     question: "What is the most popular meal for Christmas in Japan?",
     answer: "KFC",
-    day: 18
+    day: 18,
   },
   {
-    question: "Which actor voiced six different roles in the film 'The Polar Express'?",
+    question:
+      "Which actor voiced six different roles in the film 'The Polar Express'?",
     answer: "Tom Hanks",
-    day: 19
+    day: 19,
   },
   {
     question: "HOH OHO is Santa's official Postcode in which country?",
     answer: "Canada",
-    day: 20
+    day: 20,
   },
   {
-    question: "What is the significance of the red and white stripes on a traditional peppermint candy cane?",
-    answer: " The red stripes are said to represent the blood and sacrifice of Jesus Christ, and the white lines symbolize his purity.",
-    day: 21
+    question:
+      "What is the significance of the red and white stripes on a traditional peppermint candy cane?",
+    answer:
+      " The red stripes are said to represent the blood and sacrifice of Jesus Christ, and the white lines symbolize his purity.",
+    day: 21,
   },
   {
-    question: "Which state in the U.S. is known for growing the most Christmas trees?",
+    question:
+      "Which state in the U.S. is known for growing the most Christmas trees?",
     answer: "Oregon",
-    day: 22
+    day: 22,
   },
   {
     question:
@@ -90,13 +97,13 @@ const triviaQuestions: ChristmasTriviaQuestion[] = [
   {
     question: "In Greek legend, who is Santa Claus derived from?",
     answer: "St. Nicholas of Myra",
-    day: 24
+    day: 24,
   },
   {
     question: "What day is it?",
     answer: "Christmas Day! 🎄🎅🎁",
-    day: 25
-  }
+    day: 25,
+  },
 ];
 
 export default function Christmas() {
@@ -124,11 +131,11 @@ export default function Christmas() {
       <div className="flex flex-col text-center">
         {currentQuestion ? (
           <div>
-            <h3 className="mb-4 text-white text-2xl font-bold">Brightmines Christmas Trivia</h3>
+            <h3 className="mb-4 text-white text-2xl font-bold">
+              Brightmines Christmas Trivia
+            </h3>
             <h2 className="text-6xl text-green-500 font-bold">
-            <div className="flex">
-            {currentQuestion.question}
-            </div>
+              <div className="flex">{currentQuestion.question}</div>
             </h2>
             <div className="mt-8 text-red-500">
               {revealed && <p className="text-5xl">{currentQuestion.answer}</p>}
