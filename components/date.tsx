2efaafb01458ad0336813918dcd60a2303f115e7
Rob Dearling <rob@nulldev.uk>
2026-05-29 12:41:01 +0100

    style: mute post date accent color

--- components/date.tsx
+++ components/date.tsx
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }: { dateString: string }) {
   const date = parseISO(dateString);
-  return <time className='text-sky-400' dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  return <time className='text-[var(--text-muted)]' dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
